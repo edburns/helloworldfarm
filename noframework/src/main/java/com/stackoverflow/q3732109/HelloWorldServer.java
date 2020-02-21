e272dd7cb442e8a1e365178e729bc6689479cf99
Ed Burns <edburns@microsoft.com>
2020-02-21 17:13:55 -0500

    Deploy and test
    modified:   .github/workflows/main.yml

--- noframework/src/main/java/com/stackoverflow/q3732109/HelloWorldServer.java
+++ noframework/src/main/java/com/stackoverflow/q3732109/HelloWorldServer.java
@@ -11,7 +11,7 @@ import com.sun.net.httpserver.HttpServer;
 public class HelloWorldServer {
 
     public static void main(String[] args) throws Exception {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        HttpServer server = HttpServer.create(new InetSocketAddress(80), 0);
         server.createContext("/test", new MyHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
